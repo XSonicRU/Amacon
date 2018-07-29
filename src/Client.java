f432183043ed6e7bdca4748bc81c392a6d8c689d
Bengeta <inovozhilov08@gmail.com>
2018-07-29 17:10:29 +0300

    client arrangements

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,18 @@
 import java.net.Socket;
+import java.util.Scanner;
 
 class Client { //Client class
     static void Start() {
         Socket chat = new Socket();
+    }
+
+    public void setIPadress(String IPadress, String port) {
+        Scanner sc = new Scanner(System.in);
+        String i = sc.nextLine();
+        IPadress = i;
+        Scanner sr = new Scanner(System.in);
+        String q = sc.nextLine();
+        port = q;
+
     }
 }
