ed17c715985a537f9e506565e047d2f8c1a459c0
Bengeta <inovozhilov08@gmail.com>
2018-07-29 18:03:30 +0300

    client arrangements

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,23 @@
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 class Client { //Client class
-    static void Start() {
-        Socket chat = new Socket();
+    private static String IPadress;
+    private static int port;
+
+    static void Start() throws IOException {
+        Socket chat = new Socket(IPadress, port);
     }
 
-    public void setIPadress(String IPadress, String port) {
+    public void setIPadress() {
         Scanner sc = new Scanner(System.in);
         String i = sc.nextLine();
         IPadress = i;
         Scanner sr = new Scanner(System.in);
-        String q = sc.nextLine();
+        int q = sc.nextInt();
         port = q;
+
 
     }
 }
